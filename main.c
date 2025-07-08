df59d7d9582d0b7b6f4eb58865039285606087a4
AlchemistDevCreate <barbosag.ivanf@gmail.com>
2025-07-07 19:16:53 -0500

    Betty Style

--- main.c
+++ main.c
@@ -9,31 +9,31 @@
  */
 int main(void)
 {
-    int len;
-    int len2;
+	int len;
+	int len2;
 
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    printf("Return value: _printf = %d | printf = %d\n", len, len2);
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	printf("Return value: _printf = %d | printf = %d\n", len, len2);
 
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
 
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 
-    len = _printf("Percent:[%%]\n");
-    len2 = printf("Percent:[%%]\n");
-    printf("Return value: _printf = %d | printf = %d\n", len, len2);
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	printf("Return value: _printf = %d | printf = %d\n", len, len2);
 
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
 
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
 
-    _printf("Len:[%d]\n", len);
-    printf("Len:[%d]\n", len2);
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 
-    return (0);
+	return (0);
 }
